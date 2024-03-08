431bed11c6b490e50b5813c047367ca279fd1960
Lukas MAUFFRÉ <lukasapplefan@gmail.com>
2024-03-08 14:30:19 +0100

    Fix PWA 2

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Service Worker registration
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
 document.querySelectorAll('.bouton-compteur').forEach(bouton => {
     bouton.addEventListener('click', function() {
         const modificateur = parseInt(this.getAttribute('data-modificateur'), 10);
@@ -9,14 +20,3 @@ document.querySelectorAll('.bouton-compteur').forEach(bouton => {
 document.getElementById('reinitialiser').addEventListener('click', function() {
     document.getElementById('valeur-compteur').innerText = '0';
 });
-
-// Service Worker registration
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, err => {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}

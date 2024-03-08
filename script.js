90e57484a2f62b43b6dce31c17dbcd4354d422ab
Lukas MAUFFRÉ <lukasapplefan@gmail.com>
2024-03-08 16:11:52 +0100

    Disable scrolling in portrait. Remove splashscreens

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ document.querySelectorAll('.bouton-compteur').forEach(bouton => {
 document.getElementById('reinitialiser').addEventListener('click', function() {
     document.getElementById('valeur-compteur').innerText = '0';
 });
+
+function adjustScrolling() {
+    const isLandscape = window.matchMedia("(orientation: landscape)").matches;
+    console.log('isLandscape', isLandscape);
+    const padding = 20; // Additional padding to consider.
+
+    // Total height required by the content plus padding.
+    const requiredHeight = document.documentElement.scrollHeight + padding;
+    // Current viewport height.
+    const viewportHeight = window.innerHeight;
+
+    // Determine if scrolling should be enabled based on content height and orientation.
+    // if ((isLandscape && requiredHeight > viewportHeight) || !isLandscape) {
+    if (isLandscape) {
+        // Enable scrolling for landscape orientation if content exceeds viewport height,
+        // or in portrait orientation regardless of content height.
+        document.documentElement.style.overflowY = 'auto'
+    } else {
+        // Disable scrolling if conditions are not met.
+        document.documentElement.style.overflowY = 'hidden';
+    }
+}
+
+// React to changes in orientation and resize.
+window.addEventListener('resize', adjustScrolling);
+window.addEventListener('orientationchange', adjustScrolling);
+
+// Initial adjustment.
+adjustScrolling();
+
+console.log('script.js loaded');

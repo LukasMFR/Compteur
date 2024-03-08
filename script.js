fc4194dfd139fc661d370a2e5f57b2a1b1ca9cd1
Lukas MAUFFRÉ <lukasapplefan@gmail.com>
2024-03-08 13:46:39 +0100

    Add PWA support. Add icons

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ document.querySelectorAll('.bouton-compteur').forEach(bouton => {
 document.getElementById('reinitialiser').addEventListener('click', function() {
     document.getElementById('valeur-compteur').innerText = '0';
 });
+
+// Code to register the service worker for your PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // Registration failed
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
